c729d7aed0453e41e9fa47068a3783ad7dca2eee
haya14busa <hayabusa1419@gmail.com>
2016-12-03 15:41:07 +0900

    Implement SumInterface

---
+++ gosum.go
@@ -0,0 +1,108 @@
+// Package gosum provides utilities for Sum/Union/Variant like types.
+package gosum
+
+import "go/types"
+
+// SumInterface represents Sum type by interface{} type.
+type SumInterface struct {
+	// underling interface type. It uses types.Named instead of types.Interface
+	// for convenience.
+	NamedInterface *types.Named
+
+	// is "internal interface" which includes unexported methods, otherwise
+	// "public" interface.
+	IsInternal bool
+
+	// Implements holds types which implement SumInterface type.
+	Implements struct {
+		Interfaces []*SumInterface
+		Pointers   []*types.Pointer
+	}
+
+	// "Implements" only holds types in PkgScope. It's especially for "public"
+	// interface.
+	PkgScope []*types.Package
+}
+
+// NewSumInterface returns SumInterface from given interface type. If the given
+// interface is "public", this function searches implemented types in pkgscope.
+// pkgscope can be nil and it's useful to pass nil if you know the interface is
+// "internal" interface.
+func NewSumInterface(namedInterface *types.Named, pkgscope []*types.Package) *SumInterface {
+	seen := make(map[*types.Named]bool)
+	return newSumInterface(seen, namedInterface, pkgscope)
+}
+
+func newSumInterface(seen map[*types.Named]bool, namedInterface *types.Named, pkgscope []*types.Package) *SumInterface {
+	if _, ok := seen[namedInterface]; ok {
+		return nil
+	}
+	seen[namedInterface] = true
+
+	i := namedInterface.Underlying().(*types.Interface)
+	if i.Empty() {
+		return nil
+	}
+
+	sum := &SumInterface{
+		NamedInterface: namedInterface,
+		IsInternal:     IsInternalInterface(i),
+	}
+
+	definedpkg := namedInterface.Obj().Pkg()
+	pkgs := pkgscope
+	if sum.IsInternal {
+		// package scope includes only defined package for "internal" interface.
+		pkgs = []*types.Package{definedpkg}
+	} else {
+		// add defined package to package scope if the scope doesn't have the
+		// defiend package.
+		found := false
+		for _, p := range pkgs {
+			found = (p == definedpkg)
+			if found {
+				break
+			}
+		}
+		if !found {
+			pkgs = append(pkgs, definedpkg)
+		}
+	}
+	sum.PkgScope = pkgs
+
+	// Test assignability of all distinct pairs of
+	// named types (T, U) where U is an interface.
+	U := namedInterface
+	for _, pkg := range pkgs {
+		for _, name := range pkg.Scope().Names() {
+			if obj, ok := pkg.Scope().Lookup(name).(*types.TypeName); ok {
+				T := obj.Type()
+				if T == U {
+					continue
+				}
+				if types.AssignableTo(T, U) { // as interface
+					if i := newSumInterface(seen, T.(*types.Named), pkgscope); i != nil {
+						sum.Implements.Interfaces = append(sum.Implements.Interfaces, i)
+					}
+				} else if !types.IsInterface(T) { // as pointer
+					if ptr := types.NewPointer(T); types.AssignableTo(ptr, U) {
+						sum.Implements.Pointers = append(sum.Implements.Pointers, ptr)
+					}
+				}
+			}
+		}
+	}
+
+	return sum
+}
+
+// IsInternalInterface returns true if the given interface has unexported
+// method.
+func IsInternalInterface(iface *types.Interface) bool {
+	for i := 0; i < iface.NumMethods(); i++ {
+		if !iface.Method(i).Exported() {
+			return true
+		}
+	}
+	return false
+}
---
+++ gosum_test.go
@@ -0,0 +1,221 @@
+package gosum
+
+import (
+	"go/ast"
+	"go/importer"
+	"go/parser"
+	"go/token"
+	"go/types"
+	"reflect"
+	"testing"
+)
+
+func TestNewSumInterface(t *testing.T) {
+	const src = `package self
+
+import (
+	"go/ast"
+)
+
+type A interface {
+	isA()
+}
+
+type B interface {
+	A
+	isB()
+}
+
+type C struct{}
+
+func (*C) isA() {}
+
+type B1 struct{}
+
+func (*B1) isA() {}
+func (*B1) isB() {}
+
+type Dep interface {
+	ast.Node
+}
+
+type Empty interface {}
+`
+
+	fset := token.NewFileSet()
+	f, err := parser.ParseFile(fset, "src.go", src, 0)
+	if err != nil {
+		t.Fatal(err)
+	}
+	conf := types.Config{Importer: importer.Default()}
+	pkg, err := conf.Check("src", fset, []*ast.File{f}, nil)
+	if err != nil {
+		t.Fatal(err) // type error
+	}
+
+	pkgscope := AllRelatedPackages(pkg)
+
+	t.Run("empty scope", func(t *testing.T) {
+		for _, name := range pkg.Scope().Names() {
+			if obj, ok := pkg.Scope().Lookup(name).(*types.TypeName); ok && types.IsInterface(obj.Type()) {
+				got := NewSumInterface(obj.Type().(*types.Named), nil)
+				if got == nil {
+					continue
+				}
+				if !(len(got.PkgScope) == 1 && got.PkgScope[0].Name() == "self") {
+					t.Errorf("PkgScope should be [src], got %v", got.PkgScope)
+				}
+				if obj.Name() == "Dep" && got.Implements.Pointers != nil {
+					t.Errorf("Dep: Implements.Pointers == %v, want nil", got.Implements.Pointers)
+				}
+			}
+		}
+	})
+
+	t.Run("ok", func(t *testing.T) {
+		for _, name := range pkg.Scope().Names() {
+			if obj, ok := pkg.Scope().Lookup(name).(*types.TypeName); ok && types.IsInterface(obj.Type()) {
+				got := NewSumInterface(obj.Type().(*types.Named), pkgscope)
+				// skip empty interface
+				if got == nil {
+					continue
+				}
+				switch obj.Name() {
+				case "A":
+					if !got.IsInternal {
+						t.Error("interface A should be internal interface, but it's public")
+					}
+					if !(len(got.PkgScope) == 1 && got.PkgScope[0].Name() == "self") {
+						t.Errorf("PkgScope should be [src], got %v", got.PkgScope)
+					}
+					{ // SumInterface.Implements.Interfaces
+						want := []string{"B"}
+						gotifaces := make([]string, len(got.Implements.Interfaces))
+						for i, iface := range got.Implements.Interfaces {
+							gotifaces[i] = iface.NamedInterface.Obj().Name()
+						}
+						if !reflect.DeepEqual(gotifaces, want) {
+							t.Errorf("Implements.Interfaces: got %v, want %v", gotifaces, want)
+						}
+					}
+					{ // SumInterface.Implements.Pointers
+						want := []string{"*src.B1", "*src.C"}
+						gotps := make([]string, len(got.Implements.Pointers))
+						for i, ptr := range got.Implements.Pointers {
+							gotps[i] = ptr.String()
+						}
+						if !reflect.DeepEqual(gotps, want) {
+							t.Errorf("Implements.Pointers: got %v, want %v", gotps, want)
+						}
+					}
+				case "B":
+					if !got.IsInternal {
+						t.Error("interface B should be internal interface, but it's public")
+					}
+					if !(len(got.PkgScope) == 1 && got.PkgScope[0].Name() == "self") {
+						t.Errorf("PkgScope should be [src], got %v", got.PkgScope)
+					}
+					{ // SumInterface.Implements.Interfaces
+						want := []string{}
+						gotifaces := make([]string, len(got.Implements.Interfaces))
+						for i, iface := range got.Implements.Interfaces {
+							gotifaces[i] = iface.NamedInterface.Obj().Name()
+						}
+						if !reflect.DeepEqual(gotifaces, want) {
+							t.Errorf("Implements.Interfaces: got %v, want %v", gotifaces, want)
+						}
+					}
+					{ // SumInterface.Implements.Pointers
+						want := []string{"*src.B1"}
+						gotps := make([]string, len(got.Implements.Pointers))
+						for i, ptr := range got.Implements.Pointers {
+							gotps[i] = ptr.String()
+						}
+						if !reflect.DeepEqual(gotps, want) {
+							t.Errorf("Implements.Pointers: got %v, want %v", gotps, want)
+						}
+					}
+				case "Dep":
+					if got.IsInternal {
+						t.Error("interface Dep should be public interface, but it's internal")
+					}
+					if !reflect.DeepEqual(got.PkgScope, pkgscope) {
+						t.Errorf("PkgScope should be %v, got %v", pkgscope, got.PkgScope)
+					}
+					covered := make(map[string]bool)
+					for _, iface := range got.Implements.Interfaces {
+						for _, ptr := range iface.Implements.Pointers {
+							covered[ptr.String()] = true
+						}
+					}
+					if len(covered) != len(got.Implements.Pointers) {
+						msg := "all covered Pointer by each Implements.Interfaces should be same with Implements.Pointers:\n got %v\nwant %v"
+						t.Errorf(msg, covered, got.Implements.Pointers)
+					}
+				default:
+					t.Errorf("got unexpected interface: %v", obj)
+				}
+			}
+		}
+	})
+
+}
+
+func TestIsInternalInterface(t *testing.T) {
+	const src = `package isinternal
+
+type publicInterface interface {
+	Public()
+}
+
+type publicInterface2 interface {
+	publicInterface
+}
+
+type internalInterface1 interface {
+	internal()
+}
+
+type internalInterface2 interface {
+	Public()
+	internal()
+}
+
+type internalInterface3 interface {
+	publicInterface
+	internal()
+}
+`
+
+	fset := token.NewFileSet()
+	f, err := parser.ParseFile(fset, "src.go", src, 0)
+	if err != nil {
+		t.Fatal(err)
+	}
+	conf := types.Config{Importer: importer.Default()}
+	pkg, err := conf.Check("src", fset, []*ast.File{f}, nil)
+	if err != nil {
+		t.Fatal(err) // type error
+	}
+
+	wants := map[string]bool{
+		"publicInterface":    false,
+		"publicInterface2":   false,
+		"internalInterface1": true,
+		"internalInterface2": true,
+		"internalInterface3": true,
+	}
+
+	for _, name := range pkg.Scope().Names() {
+		if obj, ok := pkg.Scope().Lookup(name).(*types.TypeName); ok && types.IsInterface(obj.Type()) {
+			typ := obj.Type().Underlying().(*types.Interface)
+			got := IsInternalInterface(typ)
+			want, ok := wants[name]
+			if !ok {
+				t.Errorf("IsInternalInterface(%s) == %v, want is not prepared", name, got)
+			} else if got != want {
+				t.Errorf("IsInternalInterface(%s) == %v, want %v", name, got, want)
+			}
+		}
+	}
+}
